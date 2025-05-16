d56024aad3b28ceb019d4b161d99afa0ee0c463c
YH-edenbro <hyun96227@gmail.com>
2025-05-16 14:10:34 +0900

    new base

--- kongeden-pjt/src/router/index.js
+++ kongeden-pjt/src/router/index.js
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '@/views/LandingView.vue'
 import BooksListView from '@/views/BooksListView.vue'
 import BookDetailView from '@/views/BookDetailView.vue'
-
+import ThreadList from '@/components/ThreadList.vue' 
+import ThreadDetailView from '@/views/ThreadDetailView.vue'
+import ThreadWriteView from '@/views/ThreadWriteView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,30 +18,19 @@ const router = createRouter({
       path: '/books',
       component: BooksList,
       children: [
-        { path: '', name: 'book', component:BooksListView}
+        { path: '', name: 'book', component:BooksListView},
+        { path: '/:bookID', name: 'bookDetail', component:BookDetailView},
       ]
     },
     {
-      path: '/books/:bookID',
-      name: 'bookDetail',
-      component: BookDetailView
-    },
-
-        {
       path: '/threads',
-      name: 'threadlistview',
-      component: ThreadsListView    
-    },
-    {
-      path: '/threads/:threadid',
-      name: 'book',
-      component: ThreadDetailView
-    },
-    {
-      path: '/books/:bookid/write',
-      name: 'threadwriteview',
-      component: ThreadWriteView
-    },
+      component: ThreadList,
+      children: [
+        {path: '', name: 'thread', component:ThreadList},
+        {path: '/:threadId', name: 'threadDetail', component: ThreadDetailView},
+        {path: '/:bookId/write', name: 'threadWrite', component: ThreadWriteView}
+      ]
+    }
   ],
 })
 
